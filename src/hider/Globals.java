16c088f89021af416f5df12d32567cc2816c965c
Luis Boto <luisboto@hotmail.es>
2021-11-02 00:00:44 +0100

    First merge version written

--- src/hider/Globals.java
+++ src/hider/Globals.java
@@ -1,7 +1,8 @@
 package hider;
 
 public abstract class Globals {
-	
-	static final int BIT_NUMBER = 4;
+
+	// RRRR-SSSS-GGGG-SSSS-BBBB-SSSS
+	static final int[] SUBSTITUTED_BITS = { 4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23 };
 
 }
--- src/hider/ImageHandler.java
+++ src/hider/ImageHandler.java
@@ -3,9 +3,9 @@ package hider;
 import java.awt.image.BufferedImage;
 
 public class ImageHandler {
-	
+
 	private BinaryConverter binaryConverter;
-	
+
 	public ImageHandler() {
 		this.binaryConverter = new BinaryConverter();
 	}
@@ -13,37 +13,63 @@ public class ImageHandler {
 	public BufferedImage hide(BufferedImage canvasImage, BufferedImage secretImage) {
 		BufferedImage resultImage = new BufferedImage(canvasImage.getWidth(), canvasImage.getHeight(),
 				BufferedImage.TYPE_INT_RGB);
-		this.merge(this.getPixelMatrix(canvasImage), this.getPixelMatrix(secretImage),
-				this.getPixelMatrix(resultImage));
-		this.setLastPixelToSize(resultImage, secretImage);
+		int[][] resultPixelMatrix = this.getPixelMatrix(resultImage);
+		this.merge(this.getPixelMatrix(canvasImage), this.getPixelMatrix(secretImage), resultPixelMatrix);
+		this.setLastPixelToSize(resultPixelMatrix, secretImage);
+		this.writeResultImagePixels(resultImage, resultPixelMatrix);
+		return resultImage;
+	}
+
+	private void writeResultImagePixels(BufferedImage resultImage, int[][] resultPixelMatrix) {
+		for (int i = 0; i < resultImage.getWidth(); i++) {
+			for (int j = 0; j < resultImage.getHeight(); j++) {
+				resultImage.setRGB(i, j, resultPixelMatrix[i][j]);
+			}
+		}
 	}
 
 	private void merge(int[][] canvas, int[][] secret, int[][] result) {
 		MatrixListHandler canvasHandler = new MatrixListHandler(canvas);
 		MatrixListHandler secretHandler = new MatrixListHandler(secret);
 		MatrixListHandler resultHandler = new MatrixListHandler(result);
-		int secretPixel;
+		int secretPixel, canvasPixel;
 		while ((secretPixel = secretHandler.getNextInt()) >= 0) {
 			resultHandler.setNextInt(this.mergeFirstPairPixels(canvasHandler.getNextInt(), secretPixel));
 			resultHandler.setNextInt(this.mergeSecondPairPixels(canvasHandler.getNextInt(), secretPixel));
 		}
+		while ((canvasPixel = canvasHandler.getNextInt()) >= 0) {
+			resultHandler.setNextInt(canvasPixel);
+		}
 	}
 
 	private int mergeFirstPairPixels(int canvasFirst, int secret) {
-		// First canvas pixel hides first 4 secret bits.
+		// First canvas pixel hides first half secret bits.
 		// Each pixel looks like this: RRRR-RRRR-GGGG-GGGG-BBBB-BBBB
 		// This operation substitutes: RRRR-SSSS-GGGG-SSSS-BBBB-SSSS
-		String canvasBits = this.binaryConverter.integerToBinaryString(canvasFirst);
-		String secretBits = this.binaryConverter.integerToBinaryString(secret);
+		char[] canvasBits = this.binaryConverter.integerToBinaryString(canvasFirst).toCharArray();
+		char[] secretBits = this.binaryConverter.integerToBinaryString(secret).toCharArray();
+		int counter = 0;
+		for (int i = 0; i < Globals.SUBSTITUTED_BITS.length; i++) {
+			canvasBits[i] = secretBits[counter];
+			counter++;
+		}
+		return Integer.parseInt(new String(canvasBits), 2);
 	}
 
 	private int mergeSecondPairPixels(int canvasSecond, int secret) {
-		// Second canvas pixel hides last 4 secret bits.
-
+		// Second canvas pixel hides last half secret bits.
+		char[] canvasBits = this.binaryConverter.integerToBinaryString(canvasSecond).toCharArray();
+		char[] secretBits = this.binaryConverter.integerToBinaryString(secret).toCharArray();
+		int counter = Globals.SUBSTITUTED_BITS.length - 1;
+		for (int i = 0; i < Globals.SUBSTITUTED_BITS.length; i++) {
+			canvasBits[i] = secretBits[counter];
+			counter++;
+		}
+		return Integer.parseInt(new String(canvasBits), 2);
 	}
 
-	private BufferedImage setLastPixelToSize(BufferedImage target, BufferedImage imgToMeasure) {
-		target.setRGB(target.getWidth(), target.getHeight(), imgToMeasure.getHeight() * imgToMeasure.getWidth());
+	private int[][] setLastPixelToSize(int[][] target, BufferedImage imgToMeasure) {
+		target[target.length][target[0].length] = imgToMeasure.getHeight() * imgToMeasure.getWidth();
 		return target;
 	}
 
